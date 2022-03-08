b762605b9a59fbb20421064e107f7907efd15483
kubodev <moisesmagna@gmail.com>
2022-03-08 20:59:16 -0300

    ejercicios tema 1 y 2

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int numero1 = 1;
+        System.out.println(numero1);
+        numero1 = 2;
+        System.out.println(numero1);
+        int arrayEnteros[];
+        arrayEnteros = new int[4];
+        arrayEnteros[0] = 33;
+        arrayEnteros[1] = 32;
+        arrayEnteros[2] = 20;
+        arrayEnteros[3] = 24;
+        System.out.println(arrayEnteros[0]);
+    }
+}
